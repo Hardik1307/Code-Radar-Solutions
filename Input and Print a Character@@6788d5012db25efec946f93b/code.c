d05d5db7fae9fa3dbeac86cb819dda293e69a725
Hardik1307 <hardik1307.becse24@chitkara.edu.in>
2025-02-06 18:23:55 +0530

    Solution to Input and Print a Character

---
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float number;
+    
+    // Input a single floating-point number
+    scanf("%f", &number);
+
+    // Print the entered float
+    printf("You entered: %f\n", number);
+
+    return 0;
+}
