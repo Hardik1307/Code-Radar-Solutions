b143e02b7df48bb50acf320e04efa9015b64c89e
Hardik1307 <singlahardik268@gmail.com>
2025-02-11 22:30:16 +0530

    Solution to Perform Bitwise AND

---
+++ Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
@@ -0,0 +1,11 @@
+// Your code here...
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    
+    printf("%d",num1&num2);
+    return 0;
+}
