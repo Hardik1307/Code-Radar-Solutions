f181e1deb44f95e42e5c15848fe9e8e389049e1d
Hardik1307 <singlahardik268@gmail.com>
2025-02-13 16:49:01 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,10 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    char ch;
+    scanf("%d %d %c",&num1,&num2,&ch);
+    printf("%d",num1 ch num2 );
+    return 0;
+    
+}
