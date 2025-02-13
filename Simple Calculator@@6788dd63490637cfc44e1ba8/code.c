322e840cce219aadc27dd46703461dde1ffe2408
Hardik1307 <singlahardik268@gmail.com>
2025-02-13 17:11:21 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -13,10 +13,14 @@ int main(){
         printf("%d",num1*num2);
     }
     else if(ch == '/'){
-        printf("%d",num1/num2);
+        if (num2 == 0) {
+            printf("Error: Division by zero");
+        } else {
+            printf("%d", num1 / num2);
+        }
     }
     else{
-        printf("ol");
+        printf("Invalid operator");
     }
     return 0;
 }
